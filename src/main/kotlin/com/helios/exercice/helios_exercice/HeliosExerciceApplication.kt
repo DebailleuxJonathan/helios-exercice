d5655395f8122ddd3cf3f7775f69909d1215d871
DebailleuxJonathan <jonathan.debailleux@edu.itescia.fr>
2025-06-14 16:03:02 +0200

    init

---
+++ src/main/kotlin/com/helios/exercice/helios_exercice/HeliosExerciceApplication.kt
@@ -0,0 +1,11 @@
+package com.helios.exercice.helios_exercice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HeliosExerciceApplication
+
+fun main(args: Array<String>) {
+	runApplication<HeliosExerciceApplication>(*args)
+}
---
+++ src/test/kotlin/com/helios/exercice/helios_exercice/HeliosExerciceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.helios.exercice.helios_exercice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HeliosExerciceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
